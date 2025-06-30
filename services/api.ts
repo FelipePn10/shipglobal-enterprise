53f12cd15c277aa4a2d56c9625b8452a779f0ef9
Felipe Panosso <felipepanosso21@gmail.com>
2025-06-30 13:43:39 -0300

    feat: add JWT token interceptor to API requests

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,28 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true, // Permite envio de cookies
 });
+
+
+export const login = async (username: string, password: string) => {
+  const response = await api.post('/auth/login', { username, password });
+  return response.data;
+};
+
+export const cadastro = async (userData: { username: string; password: string; email: string }) => {
+  const response = await api.post('/auth/cadastro', userData);
+  return response.data;
+};
+
+// Outros endpoints do backend
+export const getSomeData = async () => {
+  const response = await api.get('/some-endpoint');
+  return response.data;
+};
+
+export default api;
