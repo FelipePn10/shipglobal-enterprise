7567b2562c5113f5f62856a0b440bcdc3319b7e7
Felipe Panosso <felipepanosso21@gmail.com>
2025-07-01 10:17:42 -0300

    chore: removed comment

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import { SessionProvider } from 'next-auth/react';
-import { Toaster } from '@/components/ui/toast';
-import { ReactNode } from 'react';
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/toast";
+import { ReactNode } from "react";
 
 interface ProvidersProps {
   children: ReactNode;
 }
-
 export default function Providers({ children }: ProvidersProps) {
   return (
     <SessionProvider>
